3333acbec97da6d4136cac84b49a75f6693e1807
yuxuanchiadm <yuxuanchiadm@126.com>
2025-08-25 00:46:58 +0800

    Refactoring code

--- src/version.py
+++ src/version.py
@@ -9,16 +9,16 @@ import logging
 
 logger = logging.getLogger(__name__)
 
-VERSION = "v0.1.1-20250824-2238-44ae026"
+VERSION = "v0.1.1-20250824-2333-98b6dc2"
 VERSION_SHORT = "v0.1.1"
 
 BUILD_INFO = {
-    "version": "v0.1.1-20250824-2238-44ae026",
-    "commit_hash": "44ae026d7640e0971554496419c540d1fb4ddf96",
-    "commit_short": "44ae026",
+    "version": "v0.1.1-20250824-2333-98b6dc2",
+    "commit_hash": "98b6dc291b1d8a0bf8f5dbff87502834973062eb",
+    "commit_short": "98b6dc2",
     "branch": "master",
-    "commit_date": "2025-08-24 22:05:44 +0800",
-    "build_time": "2025-08-24T22:38:12.087484",
+    "commit_date": "2025-08-24 22:54:39 +0800",
+    "build_time": "2025-08-24T23:33:30.028730",
     "python_version": "3.11.4 (tags/v3.11.4:d2340ef, Jun  7 2023, 05:45:37) [MSC v.1934 64 bit (AMD64)]",
     "platform": "win32"
 }
