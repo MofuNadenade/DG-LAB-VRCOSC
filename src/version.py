8c7e7c41d831a65b417ef2c4bc2314a01c5fa56e
yuxuanchiadm <yuxuanchiadm@126.com>
2025-08-31 05:27:52 +0800

    Refactoring code

--- src/version.py
+++ src/version.py
@@ -9,16 +9,16 @@ from typing import Dict
 
 logger = logging.getLogger(__name__)
 
-VERSION = "v0.1.1-20250831-0141-49b1c65"
+VERSION = "v0.1.1-20250831-0526-9092a3b"
 VERSION_SHORT = "v0.1.1"
 
 BUILD_INFO = {
-    "version": "v0.1.1-20250831-0141-49b1c65",
-    "commit_hash": "49b1c65397d16f07ce61f106d14f6bb8205b25d7",
-    "commit_short": "49b1c65",
+    "version": "v0.1.1-20250831-0526-9092a3b",
+    "commit_hash": "9092a3b0ab3a4b46ace2f9e468da26e86d7b0ce0",
+    "commit_short": "9092a3b",
     "branch": "master",
-    "commit_date": "2025-08-30 13:38:16 +0800",
-    "build_time": "2025-08-31T01:41:03.493592",
+    "commit_date": "2025-08-31 05:24:25 +0800",
+    "build_time": "2025-08-31T05:26:36.689208",
     "python_version": "3.11.4 (tags/v3.11.4:d2340ef, Jun  7 2023, 05:45:37) [MSC v.1934 64 bit (AMD64)]",
     "platform": "win32"
 }
