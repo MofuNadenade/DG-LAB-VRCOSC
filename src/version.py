1e8bcc3b8698ba68a5905ef17a461686588ef5cb
MofuNadenade <mofunadenade@126.com>
2025-09-10 01:11:59 +0800

    Release v0.2.6

--- src/version.py
+++ src/version.py
@@ -9,16 +9,16 @@ from typing import Dict
 
 logger = logging.getLogger(__name__)
 
-VERSION = "v0.2.5"
+VERSION = "v0.2.5-20250910-0051-f9821b1"
 VERSION_SHORT = "v0.2.5"
 
 BUILD_INFO = {
-    "version": "v0.2.5",
-    "commit_hash": "3d1e62caa09dcbc8a3d920c8be67ef7a4f8eaf87",
-    "commit_short": "3d1e62c",
+    "version": "v0.2.5-20250910-0051-f9821b1",
+    "commit_hash": "f9821b1c545bdf36bf0701f97e3115ae7469f098",
+    "commit_short": "f9821b1",
     "branch": "master",
-    "commit_date": "2025-09-09 23:58:28 +0800",
-    "build_time": "2025-09-10T00:22:01.528406",
+    "commit_date": "2025-09-10 00:48:02 +0800",
+    "build_time": "2025-09-10T00:51:03.265549",
     "python_version": "3.13.7 (tags/v3.13.7:bcee1c3, Aug 14 2025, 14:15:11) [MSC v.1944 64 bit (AMD64)]",
     "platform": "win32"
 }
